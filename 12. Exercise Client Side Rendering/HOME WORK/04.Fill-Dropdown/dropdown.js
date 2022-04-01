6337a5f576da5b40f02bde0084635ac774456f48
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-04-01 17:04:47 +0200

    Client side rendering

---
+++ 12. Exercise Client Side Rendering/HOME WORK/04.Fill-Dropdown/dropdown.js
@@ -0,0 +1,56 @@
+import { html, render } from './node_modules/lit-html/lit-html.js'
+
+const selectTemplate = (items) => html`
+<select id="menu">
+    ${items.map(i => html`<option value="${i._id}">${i.text}</option>`)}
+</select>`
+
+// start:
+// add event listener
+// call getData
+const url = 'http://localhost:3030/jsonstore/advanced/dropdown'
+const root = document.querySelector('div');
+document.querySelector('form').addEventListener('submit', addItem)
+getData();
+
+
+// getData:
+// fetch and parse
+// call template
+async function getData() {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    update(Object.values(data));
+}
+
+
+// update:
+// render template
+function update(items) {
+    // const result = selectTemplate([{ text: 'Sofia', _id: '123asd' }]);
+    const result = selectTemplate(items);
+    render(result, root);
+}
+
+
+// add  item:
+// read input
+// make POST request
+// on success, call getData
+async function addItem(ev) {
+    ev.preventDefault();
+    const text = document.getElementById('itemText').value;
+
+    const res = await fetch(url, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ text })
+    });
+
+    if (res.ok) {
+        getData();
+    }
+}
